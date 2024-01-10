58570b59c8af5be08602eb9b822338065950d8c2
Tossan99 <>
2024-01-10 15:59:07 +0000

    Add SECRET_KEY varible to settings.py

--- osrs_tips/settings.py
+++ osrs_tips/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h-ph3c=58p&+%g-^tqah*1hr18rr%@9(3oisd2x1!t+vct766('
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
