e4f209d69677593fd4bec9878d71e09a7fa99e79
Tossan99 <>
2024-01-12 20:49:21 +0000

    Add style to base and index html pages

--- forum/urls.py
+++ forum/urls.py
@@ -2,7 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='posts'),
+    path('', views.PostList.as_view(), name='home'),
     path('post_create/', views.post_create, name='post_create'),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
 ]
--- osrs_tips/urls.py
+++ osrs_tips/urls.py
@@ -21,5 +21,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('summernote/', include('django_summernote.urls')),
-    path("", include("forum.urls"), name="forum-urls"),
+    path("", include("forum.urls"), name="home"),
 ]
