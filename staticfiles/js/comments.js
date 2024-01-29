d13c6bcf76744c135ebe5f4506de9d931f2c030e
Tossan99 <>
2024-01-29 10:53:34 +0000

    Set debug to false

--- staticfiles/js/comments.js
+++ staticfiles/js/comments.js
@@ -4,25 +4,38 @@ const commentForm = document.getElementById("commentForm");
 const submitButton = document.getElementById("submitButton");
 
 const deleteModal = new bootstrap.Modal(document.getElementById("deleteModal"));
-const deleteButtons = document.getElementsByClassName("btn-delete");
+const commentDeleteButtons = document.getElementsByClassName("btn-delete-comment");
+const postDeleteButtons = document.getElementsByClassName("btn-delete-post");
 const deleteConfirm = document.getElementById("deleteConfirm");
+const deleteModalText = document.getElementById("deleteModalText");
 
 for (let button of editButtons) {
-  button.addEventListener("click", (e) => {
-    let commentId = e.target.getAttribute("comment_id");
-    let commentContent = document.getElementById(
-      `comment${commentId}`
-    ).innerText;
-    commentText.value = commentContent;
-    submitButton.innerText = "Update";
-    commentForm.setAttribute("action", `edit_comment/${commentId}`);
-  });
+    button.addEventListener("click", (e) => {
+        let commentId = e.target.getAttribute("comment_id");
+        let commentContent = document.getElementById(
+        `comment${commentId}`
+        ).innerText;
+        commentText.value = commentContent;
+        submitButton.innerText = "Update";
+        commentForm.setAttribute("action", `edit_comment/${commentId}`);
+    });
 }
 
-for (let button of deleteButtons) {
-  button.addEventListener("click", (e) => {
-    let commentId = e.target.getAttribute("comment_id");
-    deleteConfirm.href = `delete_comment/${commentId}`;
-    deleteModal.show();
-  });
+for (let button of commentDeleteButtons) {
+    button.addEventListener("click", (e) => {
+        let commentId = e.target.getAttribute("comment_id");
+        deleteModalText.innerText = "Are you sure you want to delete this comment? This action cannot be undone!"
+        deleteConfirm.href = `delete_comment/${commentId}`;
+        deleteModal.show();
+        
+    });
+}
+
+for (let button of postDeleteButtons) {
+    button.addEventListener("click", (e) => {
+        let postId = e.target.getAttribute("post_id");
+        deleteModalText.innerText = "Are you sure you want to delete this post? This action cannot be undone!"
+        deleteConfirm.href = `delete_post/`;
+        deleteModal.show();
+    });
 }
